429854b9cec05901a0bba9357a96ef58819167dd
f_tomes <tomes@cngroup.dk>
2021-12-18 11:00:29 +0100

    error 404 page added

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const ErrorPage = () => {
   return (
-    <div>
-      404 page not found
-    </div>
+    <section id='error-page' className='pt-16 flex flex-col justify-start items-center'>
+      <Link to='/' className='text-center text-2xl'>
+        <h1 className='text-8xl'>404 - page not found</h1>
+        Back to homepage
+      </Link>
+    </section>
   )
 }
